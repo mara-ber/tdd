342f84cbe4490a29089d90e6d02231ca7240a2d5
Marina <mara.berezin@gmail.com>
2021-10-13 17:21:51 +0100

    third test - scissors vs. paper; function needs to be refactored

--- index.test.js
+++ index.test.js
@@ -1,5 +1,7 @@
-function rps(left) {
-    return left === "rock" ? "left" : "right";
+function rps(left, right) {
+    return left === "rock"
+        ? "left"
+        : (right === "paper" ? "left" : "right");
 }
 
 describe("rock, paper, scissors", () => {
@@ -19,5 +21,14 @@ describe("rock, paper, scissors", () => {
         const result = rps(left, right);
 
         expect(result).toBe("right");
+    });
+
+    it("should say left wins for scissors vs. paper", () => {
+        const left = "scissors";
+        const right = "paper";
+
+        const result = rps(left, right);
+
+        expect(result).toBe("left");
     });
 })

294e61e143f3c16d2f7e327e1481a56a2db58412
Marina <mara.berezin@gmail.com>
2021-10-13 17:55:53 +0100

    fifth test - paper vs. rock

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 function rps(left, right) {
-    return left === "rock" || right === "paper"
+    return left === "rock" || right === "paper" || (left === "paper" && right === "rock")
         ? "left"
         : "right";
 }
@@ -39,5 +39,14 @@ describe("rock, paper, scissors", () => {
         const result = rps(left, right);
 
         expect(result).toBe("right");
+    });
+
+    it("should say left wins for paper vs. rock", () => {
+        const left = "paper";
+        const right = "rock";
+
+        const result = rps(left, right);
+
+        expect(result).toBe("left");
     });
 })
